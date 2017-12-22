3155c8bc6f274400846b1dd02ba4160d94797bc5
Mesut OZTURK <mesutoztrk@msn.com>
2017-12-22 13:46:37 +0300

    5 angularjs

--- SepetUygulamasi/Controllers/HomeController.cs
+++ SepetUygulamasi/Controllers/HomeController.cs
@@ -30,5 +30,62 @@ namespace SepetUygulamasi.Controllers
             }).ToList();
             return Json(model, JsonRequestBehavior.AllowGet);
         }
+        [HttpPost]
+        public JsonResult SiparisOlustur(List<SiparisViewModel> model)
+        {
+            if (model == null)
+                return Json("",JsonRequestBehavior.AllowGet);
+            NorthContext db = new NorthContext();
+            using (var tran = db.Database.BeginTransaction())
+            {
+                try
+                {
+                    var siparis = new Order()
+                    {
+                        CustomerID = "DUMON",
+                        EmployeeID = 1,
+                        OrderDate = DateTime.Now,
+                        RequiredDate = DateTime.Now.AddDays(7),
+                        ShipVia = 1,
+                        Freight = 59,
+                        ShipName = "Lucky S",
+                        ShipRegion = "Çorum",
+                        ShipCity = "Sungurlu",
+                        ShipCountry = "USÇ"
+                    };
+                    db.Orders.Add(siparis);
+                    db.SaveChanges();
+                    foreach (var item in model)
+                    {
+                        db.Order_Details.Add(new Order_Detail
+                        {
+                            OrderID = siparis.OrderID,
+                            ProductID = item.ProductID,
+                            Discount = 0,
+                            Quantity = item.Count,
+                            UnitPrice = item.UnitPrice
+                        });
+                    }
+                    db.SaveChanges();
+                    tran.Commit();
+                    var msg = new
+                    {
+                        success = true,
+                        message = $"{siparis.OrderID} nolu sipariş başarıyla oluşturuldu"
+                    };
+                    return Json(msg, JsonRequestBehavior.AllowGet);
+                }
+                catch (Exception ex)
+                {
+                    tran.Rollback();
+                    var msg = new
+                    {
+                        success = false,
+                        message = "Sipariş oluşturulurken bir hata oluştu " + ex.Message
+                    };
+                    return Json(msg, JsonRequestBehavior.AllowGet);
+                }
+            }
+        }
     }
 }
---
+++ SepetUygulamasi/Models/SiparisViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SepetUygulamasi.Models
+{
+    public class SiparisViewModel
+    {
+        public int ProductID { get; set; }
+        public short Count { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
